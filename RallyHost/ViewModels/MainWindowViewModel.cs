c45a6fd3a87a5c0c27d07f910d106749fd5267e2
nkanf-dev <n.kan.f@outlook.com>
2025-03-05 20:45:05 +0800

    created some views

--- RallyHost/ViewModels/MainWindowViewModel.cs
+++ RallyHost/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,22 @@
-﻿namespace RallyHost.ViewModels
+﻿using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace RallyHost.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        
+        [ObservableProperty] private UserControl _currentView = new HomeView();
+
+        [RelayCommand]
+        public void ToggleView(string viewName)
+        {
+            CurrentView = viewName switch
+            {
+                "Home" => new HomeView(),
+                "Settings" => new SettingsView(),
+                _ => new HomeView()
+            };
+        }
     }
 }
---
+++ RallyHost/Views/HomeView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace RallyHost;
+
+public partial class HomeView : UserControl
+{
+    public HomeView()
+    {
+        InitializeComponent();
+    }
+}
